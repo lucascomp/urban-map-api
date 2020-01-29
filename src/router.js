bdf44394f3bfc2d3581ca28bc9efb5a9fc7f3a3e
lucascomp <lucas_comp@outlook.com>
2020-01-29 00:22:48 -0300

    Accessibilities route

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router');
+const accessibility = require('./controllers/accessibility');
 const marker = require('./controllers/marker');
 const user = require('./controllers/user');
 
@@ -15,5 +16,7 @@ router.post('/reset-password', user.resetPassword);
 router.put('/signup', user.signup);
 
 router.get('/markers', marker.getAll);
+
+router.get('/accessibilities', accessibility.getAll);
 
 module.exports = router;
