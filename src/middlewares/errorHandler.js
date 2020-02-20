481b05107a77bea78631edb4460d82081282e040
lucascomp <lucas_comp@outlook.com>
2020-02-19 23:07:17 -0300

    Restricted session

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = async (ctx, next) => {
     await next();
   } catch (error) {
     ctx.status = error.status || 500;
-    ctx.body = { message: error.message };
+    ctx.body = { message: error.message, status: error.status };
     ctx.app.emit('error', error, ctx);
   }
 };
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const accessibility = require('./controllers/accessibility');
 const marker = require('./controllers/marker');
 const user = require('./controllers/user');
+const { restrictedSession } = require('./middlewares/restrictedSession');
 
 const router = new Router();
 
@@ -15,13 +16,13 @@ router.get('/login/facebook', user.loginWithFacebook);
 router.get('/login/facebook/callback', user.loginCallback('facebook'));
 router.get('/login/google', user.loginWithGoogle);
 router.get('/login/google/callback', user.loginCallback('google'));
-router.get('/logout', user.logout);
+router.get('/logout', restrictedSession, user.logout);
 router.post('/reset-password', user.resetPassword);
 router.put('/signup', user.signup);
 
-router.get('/markers', marker.getAll);
-router.put('/markers', marker.create);
+router.get('/markers', restrictedSession, marker.getAll);
+router.put('/markers', restrictedSession, marker.create);
 
-router.get('/accessibilities', accessibility.getAll);
+router.get('/accessibilities', restrictedSession, accessibility.getAll);
 
 module.exports = router;
